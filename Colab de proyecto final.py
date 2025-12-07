59a55863fecd9c6d9cd1dac4de1021a41bb0ba23
orianalazolaura <a20222230@pucp.edu.pe>
2025-12-07 00:27:38 -0500

    Refactor Streamlit project for Romanticism theme

--- Colab de proyecto final.py
+++ Colab de proyecto final.py
@@ -117,7 +117,7 @@ elif pagina_seleccionada == '🐇Diario de un genio incomprendido':
         return f"""
     entrada = """
 "🌴EL DIARIO DEL GENIO INCOMPRENDIDO 🌴"
-💭 Hoy me siento: {sentimiento} 🌸
+"💭 Hoy me siento: {sentimiento} 🌸"
 No sé si es un glitch en mi matrix interior o un destello de claridad en la neón-tormenta.
 
 ✨ Deseo más profundo: {deseo} ✨
@@ -132,6 +132,7 @@ Cada nube refleja mi aura retro-futurista, vibrando en 80s pastel y synthwave.
 st.text(entrada)
     if st.button("🌟 Generar Diario"):
         st.markdown(f"<pre style='color:#FF77FF;'>{generar_diario_vaporwave(sentimiento, deseo, clima)}</pre>", unsafe_allow_html=True)
+
 
 
 
